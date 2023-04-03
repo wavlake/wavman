b688c4aecdeacec4213aa62b7c285be249e1183c
Josh Remaley <joshremaley@gmail.com>
2023-04-03 14:56:20 -0500

    mock data utils

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,17 +1,39 @@
+import { Event, UnsignedEvent, signEvent, generatePrivateKey, getPublicKey, getEventHash } from "nostr-tools"
 // event kind 32123
-
-export interface WavlakeEvent {
+export interface WavlakeEventContent {
   title: string;
   description: string;
   link: string;
   author: string;
-  medium: string; // use an enum? 
+  type: string; // MIME type
   guid: string;
   creator: string;
-  pubDate: string; // use Date type instead?
+  pubDate: string;
   enclosure: string;
-  episode: number; // always a number?
-  season: number; // always a number?
   duration: string;
   version: string;
 };
+
+export interface SignedEventKind32123 extends Omit<Event, 'kind'> {
+  kind: 32123,
+};
+
+export interface UnsignedEventKind32123 extends Omit<UnsignedEvent, 'kind'> {
+  kind: 32123,
+};
+
+// generate a throwaway private key for mock data and mock comments
+export const publisherPrivateKey = generatePrivateKey();
+export const commenterPrivateKey = generatePrivateKey();
+
+export const signTrackEvent = (event: UnsignedEventKind32123): SignedEventKind32123 => ({
+  ...event,
+  id: getEventHash(event),
+  sig: signEvent(event, publisherPrivateKey),
+});
+
+export const signCommentEvent = (event: UnsignedEvent): Event => ({
+  ...event,
+  id: getEventHash(event),
+  sig: signEvent(event, commenterPrivateKey)
+});
--- src/shared/mockData.ts
+++ src/shared/mockData.ts
@@ -1,17 +1,34 @@
-import { WavlakeEvent } from "./interfaces";
+import { UnsignedEventKind32123, SignedEventKind32123, signCommentEvent, commenterPrivateKey, signTrackEvent, WavlakeEventContent, publisherPrivateKey } from "./interfaces";
+import { Event } from "nostr-tools";
 
-export const sampleEvent: WavlakeEvent = {
+const wavlakeEventContent: WavlakeEventContent = {
   title: "I Can Be",
   description: "I Can Be",
   link: "https://www.wavlake.com/album/9cbf460f-60e2-4271-87a2-93b8e0520a95",
   author: "Official DETOX Music",
-  medium: "music",
+  type: "music",
   guid: "87c0d2f9-909f-48ce-8b02-3986aa9b0013",
   creator: "Official DETOX Music",
   pubDate: "Sat, 04 Mar 2023 20:35:29 GMT",
   enclosure: "https://d12wklypp119aj.cloudfront.net/track/87c0d2f9-909f-48ce-8b02-3986aa9b0013.mp3",
-  episode: 1,
-  season: 1,
   duration: "00:04:03",
   version: "1.0"
 };
+
+export const unsignedMockTrackEvent: UnsignedEventKind32123 = {
+  content: JSON.stringify(wavlakeEventContent),
+  kind: 32123,
+  tags: [],
+  created_at: 12345,
+  pubkey: publisherPrivateKey,
+};
+
+export const signedMockTrackEvent: SignedEventKind32123 = signTrackEvent(unsignedMockTrackEvent)
+
+export const comments: Event[] = new Array(9).map(comment => signCommentEvent({
+  content: "test comment",
+  kind: 123,
+  tags: [["e", ]],
+  created_at: 12345 + comment,
+  pubkey: commenterPrivateKey,
+}))
