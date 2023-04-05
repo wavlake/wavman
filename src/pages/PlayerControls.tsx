6ae2f28b1860a63b15322018214254d0edc3bb02
Josh Remaley <joshremaley@gmail.com>
2023-04-05 15:44:32 -0500

    working hardware controls

--- src/pages/PlayerControls.tsx
+++ src/pages/PlayerControls.tsx
@@ -1,8 +1,10 @@
-import { MouseEventHandler, useState } from "react";
-import { COMMENTS_VIEW, PageView } from "./shared";
+import { useState } from "react";
+import { COMMENTS_VIEW, PageView, PLAYER_VIEW, SPLASH_VIEW, ZAP_VIEW } from "./shared";
 
 type Actions = "PLAY" | "PAUSE" | "ZAP" | "SKIP" | ">" | "<";
 
+type ToggleViewHandler = (pageView: PageView) => void;
+type ActionHandler = () => void | ToggleViewHandler;
 const PlayerControls: React.FC<{
   isPlaying: boolean;
   pageView: PageView;
@@ -12,36 +14,51 @@ const PlayerControls: React.FC<{
   toggleViewHandler: (pageView: PageView) => void
 }> = ({ isPlaying, pageView, playHandler, skipHandler, zapHandler, toggleViewHandler }) => {
   const [selectedIndex, setSelectedIndex] = useState(0); 
-  const actions: Actions[] = ["PLAY", "ZAP", "SKIP", ">"];
-  const actionHandlerMap: Record<Actions, MouseEventHandler<HTMLButtonElement>> = {
+  const commentViewActions: Actions[] = ["<", "PLAY", "ZAP", "SKIP"];
+  const pageViewActions: Actions[] = ["PLAY", "ZAP", "SKIP", ">"];
+  const zapViewActions: Actions[] = [];
+  const splashViewActions: Actions[] = [];
+  const actionHandlerMap: Record<Actions, ActionHandler> = {
     PLAY: playHandler,
     PAUSE: playHandler,
     ZAP: zapHandler,
     SKIP: skipHandler,
     ">": () => toggleViewHandler(COMMENTS_VIEW),
-    "<": () => toggleViewHandler(COMMENTS_VIEW),
-  }
+    "<": () => toggleViewHandler(PLAYER_VIEW),
+  };
+  const pageViewActionMap: Record<PageView, Actions[]> = {
+    [PLAYER_VIEW]: pageViewActions,
+    [COMMENTS_VIEW]: commentViewActions,
+    [ZAP_VIEW]: zapViewActions,
+    [SPLASH_VIEW]: splashViewActions,
+  };
+  const currentActions = pageViewActionMap[pageView];
+  const centerHandler = () => actionHandlerMap[currentActions[selectedIndex]]();
+
+  const calcMoveIndexRight = (index: number) => index + 1 >= currentActions.length ? index : index + 1;
+  const calcMoveIndexLeft = (index: number) => index === 0 ? index : index - 1;
 
   const upHandler = () => {
-  };
-  const leftHandler = () => {
+    
   };
   const downHandler = () => {
-  };
-  const rightHandler = () => {
-  };
-  const centerHandler = () => {
+  
   };
 
+  const leftHandler = () => setSelectedIndex((selectedIndex) => calcMoveIndexLeft(selectedIndex));
+  const rightHandler = () => setSelectedIndex((selectedIndex) => calcMoveIndexRight(selectedIndex));
+
+  // bg-green-700
+  // bg-amber-200
   return (
     <div>
-      {actions.map((action, index) => (
+      {pageViewActionMap[pageView].map((action, index) => (
         <div
           key={action}
-          className={selectedIndex === index ? 'border-2 border-amber-500' : 'bg-green-200'}
+          className={selectedIndex === index ? 'border-2 border-amber-500' : ''}
         >{action}</div>
       ))}
-      <button className="bg-green-700" onClick={upHandler}>Up</button>
+      <button className="" onClick={upHandler}>Up</button>
       <button onClick={leftHandler}>Left</button>
       <button onClick={downHandler}>Down</button>
       <button onClick={rightHandler}>Right</button>
