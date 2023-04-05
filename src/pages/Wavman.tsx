ec94fb182019d66ae1bc2ede490d02c4a1781528
Josh Remaley <joshremaley@gmail.com>
2023-04-05 12:51:19 -0500

    refine randomChar filter

--- src/pages/Wavman.tsx
+++ src/pages/Wavman.tsx
@@ -31,10 +31,16 @@ const signComment = (content: string, parentTrack: Event): Event => {
   };
 };
 
-const randomString = (length: number) => Array.from(Array(length), () => Math.floor(Math.random() * 36).toString(36)).join('');
+const randomSHA256String = (length: number) => {
+  const alphanumericString = Array.from(Array(length + 30), () => Math.floor(Math.random() * 36).toString(36)).join('');
+  const SHA256Regex = /[^A-Fa-f0-9-]/g;
+  return alphanumericString.replace(SHA256Regex, '').slice(0, length);
+}
 
 const Wavman: React.FC<{}> = ({}) => {
-  const [randomChar, setRandomChar] = useState<string[]>(Array.from(randomString(4)));
+  // 4 characters returns ~90-130 tracks
+  // will need to re-raondomize this filter once the user reaches the end of the list
+  const [randomChar, setRandomChar] = useState<string[]>(Array.from(randomSHA256String(4)));
   const { data: tracks, loading: tracksLoading } = useRelayList([
     { kinds: [32123], ["#f"]: randomChar},
   ]);
