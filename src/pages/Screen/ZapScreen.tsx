bfa8dd048c8bd7cf6e46ec0839898797e0f99032
Josh Remaley <joshremaley@gmail.com>
2023-04-07 13:27:05 -0500

    add zap screen

--- src/pages/Screen/ZapScreen.tsx
+++ src/pages/Screen/ZapScreen.tsx
@@ -1,22 +1,20 @@
-import QRCode from "qrcode";
+import { useNIP07Login } from "@/nostr/useNip07Login";
 import { useEffect, useState } from "react";
+import { useFormContext } from "react-hook-form";
 
 const ZapScreen: React.FC<{
-  paymentRequest: string;
-}> = ({ paymentRequest }) => {
-  const [qrImage, setQrImage] = useState<string | undefined>();
-  useEffect(() => {
-    if (paymentRequest.length === 0) return;
-    QRCode.toDataURL(`lightning:${paymentRequest}`)
-      .then((img) => setQrImage(img))
-      .catch((e) => console.log(`${e}`));
-  }, [paymentRequest]);
+}> = ({ }) => {
+  const { publicKey } = useNIP07Login();
+  const { register } = useFormContext();
 
   return (
     <div className="m-4 justify-self-center">
-      {paymentRequest.length ? (
-        <img src={qrImage} height={300} width={300} />
-      ) : "Loading QR Code..."}
+      {publicKey && 
+        <>
+          <input {...register("content")} />
+          <input {...register("satAmount")} type="number" step="1" />
+        </>
+      }
     </div>
   );
 };
