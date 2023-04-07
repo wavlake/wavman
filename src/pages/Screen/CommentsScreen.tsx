6914a220a18769adad474b25aa2ae71648a1c208
Josh Remaley <joshremaley@gmail.com>
2023-04-07 13:27:37 -0500

    remov input from comment screen

--- src/pages/Screen/CommentsScreen.tsx
+++ src/pages/Screen/CommentsScreen.tsx
@@ -1,6 +1,4 @@
-import { useNIP07Login } from "@/nostr/useNip07Login";
 import { Event } from "nostr-tools";
-import { useFormContext } from "react-hook-form";
 
 const Comment: React.FC<{ comment: Event }> = ({ comment }) => (
   <div>{comment.content}</div>
@@ -10,18 +8,10 @@ const CommentsScreen: React.FC<{
   loading: boolean;
   comments: Event[];
 }> = ({ loading, comments }) => {
-  const { register } = useFormContext();
-
   if (loading) return <div>Comments Loading Screen</div>;
-  const { publicKey } = useNIP07Login();
+
   return (
     <div className="">
-      {publicKey && 
-        <>
-          <input {...register("comment")} />
-          <button type="submit">Add Comment</button>
-        </>
-      }
       {!comments.length ? (
         <div>No comments yet...</div>
       ) : (
