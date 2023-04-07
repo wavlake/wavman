aad08b76f0e9449426d893d63de39202952eee34
Josh Remaley <joshremaley@gmail.com>
2023-04-07 13:27:18 -0500

    add QR screen

---
+++ src/pages/Screen/QRScreen.tsx
@@ -0,0 +1,24 @@
+import QRCode from "qrcode";
+import { useEffect, useState } from "react";
+
+const QRScreen: React.FC<{
+  paymentRequest: string;
+}> = ({ paymentRequest }) => {
+  const [qrImage, setQrImage] = useState<string | undefined>();
+  useEffect(() => {
+    if (paymentRequest.length === 0) return;
+    QRCode.toDataURL(`lightning:${paymentRequest}`)
+      .then((img) => setQrImage(img))
+      .catch((e) => console.log(`${e}`));
+  }, [paymentRequest]);
+
+  return (
+    <div className="m-4 justify-self-center">
+      {paymentRequest.length ? (
+        <img src={qrImage} height={300} width={300} />
+      ) : "Loading QR Code..."}
+    </div>
+  );
+};
+
+export default QRScreen;
