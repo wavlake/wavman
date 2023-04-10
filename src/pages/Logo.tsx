e84fee6520c93532c902fb7fdd2fdca3f06bba53
Josh Remaley <joshremaley@gmail.com>
2023-04-10 12:27:16 -0500

    revert logo image

--- src/pages/Logo.tsx
+++ src/pages/Logo.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 const Logo: React.FC<{}> = () => (
   <div className="mx-auto flex justify-center space-x-2">
     <div className="">
-      <Image className="h-8" src={"icon.svg"} alt="logo" />
+      <img className="h-8" src="icon.svg" alt="logo" />
     </div>
     <div className="text-3xl tracking-tighter">WAVMAN</div>
   </div>
