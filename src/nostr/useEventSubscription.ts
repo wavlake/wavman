798dfd3876d135d06b02224e43d5502ec517c41a
Josh Remaley <joshremaley@gmail.com>
2023-04-18 14:09:04 -0500

    add invoice to subkey for zap receipts

--- src/nostr/useEventSubscription.ts
+++ src/nostr/useEventSubscription.ts
@@ -8,14 +8,15 @@ import { RelayStatus } from "./relayProvider";
 export const useEventSubscription = (
   filter: Filter[],
   skip = false,
-  opts?: SubscriptionOptions
+  opts?: SubscriptionOptions,
+  subKeyModifier?: string,
 ) => {
   const { relay, reconnect } = useRelay();
   const [loading, setLoading] = useState(false);
   const [allEvents, setAllEvents] = useState<Event[]>([]);
   const subKey = JSON.stringify(filter);
   const { data, error } = useSWRSubscription(
-    subKey,
+    subKey + subKeyModifier || "",
     (_key, { next }: SWRSubscriptionOptions<Event, Error>) => {
       const subHandler = () => {
         setLoading(true);
