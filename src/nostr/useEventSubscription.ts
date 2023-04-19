b3ecc3adc7167f9d5108e23f40b4ce81965cbb9c
Josh Remaley <joshremaley@gmail.com>
2023-04-18 19:43:23 -0500

    revert event subscription

--- src/nostr/useEventSubscription.ts
+++ src/nostr/useEventSubscription.ts
@@ -3,58 +3,41 @@ import { Filter, SubscriptionOptions, Event } from "nostr-tools";
 import { useState } from "react";
 import useSWRSubscription from "swr/subscription";
 import type { SWRSubscriptionOptions } from "swr/subscription";
-import { RelayStatus } from "./relayProvider";
 
 export const useEventSubscription = (
   filter: Filter[],
   skip = false,
-  opts?: SubscriptionOptions,
-  subKeyModifier?: string,
+  opts?: SubscriptionOptions
 ) => {
-  const { relay, reconnect } = useRelay();
+  const { relay } = useRelay();
   const [loading, setLoading] = useState(false);
   const [allEvents, setAllEvents] = useState<Event[]>([]);
   const subKey = JSON.stringify(filter);
   const { data, error } = useSWRSubscription(
-    subKey + subKeyModifier || "",
+    subKey,
     (_key, { next }: SWRSubscriptionOptions<Event, Error>) => {
-      const subHandler = () => {
-        setLoading(true);
-        if (relay && !skip) {
-          const sub = relay.sub(filter, opts);
-          sub.on("event", (event) => {
-            setAllEvents((allEvents) => [...allEvents, event]);
-            next(null, event);
-          });
-          sub.on("eose", () => {
-            setLoading(false);
-          });
-          // cleanup function
-          return () => {
-            sub.unsub();
-            setAllEvents([]);
-            setLoading(false);
-          };
-        }
-        setLoading(false);
-        return () => {
-          setAllEvents([]);
-          setLoading(false);
-        };
-      };
-
-      if (!relay) return
-      if (relay.status === RelayStatus.Open || relay.status === RelayStatus.Connecting) {
-        return subHandler();
-      } else {
-        reconnect().then(res => {
-          subHandler();
+      setLoading(true);
+      if (relay && !skip) {
+        const sub = relay.sub(filter, opts);
+        sub.on("event", (event) => {
+          setAllEvents((allEvents) => [...allEvents, event]);
+          next(null, event);
         });
+        sub.on("eose", () => {
+          setLoading(false);
+        });
+        // cleanup function
         return () => {
+          sub.unsub();
           setAllEvents([]);
           setLoading(false);
         };
       }
+      setLoading(false);
+      return () => {
+        setAllEvents([]);
+        setLoading(false);
+      };
     }
   );
   return { lastEvent: data, allEvents, error, loading };
