2d14623cd70d1cfa3a20df7336a823fedd66cf02
Josh Remaley <joshremaley@gmail.com>
2023-04-19 11:48:10 -0500

    type default content

--- src/pages/WavmanPlayer.tsx
+++ src/pages/WavmanPlayer.tsx
@@ -170,13 +170,14 @@ const WavmanPlayer: React.FC<{
   }, [kind1NowPlaying, relay]);
 
   const [kind32123NowPlaying] = kind32123Events || [];
+  const defaultTrackInfo: Partial<WavlakeEventContent> = {
+    title: "",
+    enclosure: "",
+    creator: "",
+    link: "",
+  }
   const trackContent: WavlakeEventContent = JSON.parse(
-    kind32123NowPlaying?.content || JSON.stringify({
-      title: "",
-      enclosure: "",
-      artist: "",
-      link: "",
-    })
+    kind32123NowPlaying?.content || JSON.stringify(defaultTrackInfo)
   );
 
   return (
